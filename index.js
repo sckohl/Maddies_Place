ed6922707a2f0fbf950889d09753204b2aa1fbd5
sckohl <sckohl@gmail.com>
2019-12-30 20:26:08 -0800

    200 response from stripe API

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ if(port == null) {
 	process.exit();
 }
 
+
+
 app.listen(port, () => console.log("Example app listening on port " + port));
 
 
@@ -18,4 +20,49 @@ app.get('/', function(req, res){
 });
 
 
+
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+
+const stripe = require('stripe')('sk_test_ZtyEjBcAgk6Y3EEA9VaNVveX00Zf9QQwxk');
+
+// In a new endpoint on your server, create a ConnectionToken and return the
+// `secret` to your app. The SDK needs the `secret` to connect to a reader.
+stripe.terminal.connectionTokens.create();
+
+
+
+// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+
+
+// const stripe = require('stripe')('sk_test_ZtyEjBcAgk6Y3EEA9VaNVveX00Zf9QQwxk');
+
+// (async () => {
+//   const paymentIntent = await stripe.paymentIntents.create({
+//     amount: 1099,
+//     currency: 'usd',
+//   });
+// })();
+
+// // Set up Stripe.js and Elements to use in checkout form
+// var style = {
+// 	base: {
+// 	  color: "#32325d",
+// 	}
+//   };
+  
+//   var card = elements.create("card", { style: style });
+//   card.mount("#card-element");
+
+
+
+//   card.addEventListener('change', ({error}) => {
+// 	const displayError = document.getElementById('card-errors');
+// 	if (error) {
+// 	  displayError.textContent = error.message;
+// 	} else {
+// 	  displayError.textContent = '';
+// 	}
+//   });
 
---
+++ www/checkoutPage.js
@@ -0,0 +1,4 @@
+// Set your publishable key: remember to change this to your live publishable key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = Stripe('pk_test_utijJKabOZWuIqDSzCPTD0Nj00ZrxVycwu');
+var elements = stripe.elements();
